e19d453de6cc9393dde67322b79c380612a5adc1
Jorge Bejar <jorge@wyeworks.com>
2012-03-18 13:41:07 -0300

    model_generator: Add support for Test::Unit (taken from https://github.com/emk/machinist/commit/660ad9e08b805e74f016830abb63d0e8d81d5075)

--- lib/generators/machinist/model/model_generator.rb
+++ lib/generators/machinist/model/model_generator.rb
@@ -2,11 +2,17 @@ module Machinist
   module Generators #:nodoc:
     class ModelGenerator < Rails::Generators::NamedBase #:nodoc:
       argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
+      class_option :test_framework, :type => :string, :aliases => "-t", :desc => "Test framework to use Machinist with"
 
       def create_blueprint
-        append_file "spec/support/blueprints.rb", "\n#{class_name}.blueprint do\n  # Attributes here\nend\n"
+        append_file blueprints_path, "\n#{class_name}.blueprint do\n  # Attributes here\nend\n"
       end
 
+      protected
+
+      def blueprints_path
+        options[:test_framework] == :rspec ? "spec/support/blueprints.rb" : "test/blueprints.rb"
+      end
     end
   end
 end
